b2e59e827bdc62f264c6567b8b50f65b3f4a9135
felipebaloneker <felipebaloneker23@gmail.com>
2021-11-04 18:07:25 -0300

    começando tela de login

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,10 +1,28 @@
-import React from  'react'
-import {View, Text} from 'react-native'
+import React, { useState } from  'react'
+import {View, Text, TextInput, Image} from 'react-native'
 
-export default function SignIn(){
+import IllustrationImg from "../../assets/illustration.png"
+import {styles} from './styles';
+
+export function SignIn(){
+    const [text, setText] = useState('');
     return(
-        <View>
-            <Text>Hello World</Text>
+        <View style ={styles.container}>
+            <Image source={IllustrationImg}
+            style={styles.image}
+            />
+        <View style={styles.content}>
+            <Text style = { styles.tittle}>
+             Organize{'\n'}
+             suas jogatinas {'\n'}
+             facilmente
+            </Text>
+             <Text style = { styles.subtitle}>
+                Crei grupos para jogar seus games {'\n'}
+                favoritos com seus amigos 
+            </Text>
+
+        </View>
         </View>
     )
 }
