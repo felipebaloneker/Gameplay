59655bdea03e9f10590e7f020abc8057be8b7aa8
hunter <hunter@hunter.local>
2021-11-09 00:02:25 -0300

    criado componente buttonIcon

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,28 +1,39 @@
 import React, { useState } from  'react'
-import {View, Text, TextInput, Image} from 'react-native'
+import {
+    View,
+    Text, 
+    Image,
+    StatusBar
+    } from 'react-native'
 
+import {ButtonIcon} from '../../components/ButtonIcon'
 import IllustrationImg from "../../assets/illustration.png"
 import {styles} from './styles';
 
 export function SignIn(){
-    const [text, setText] = useState('');
+    
     return(
         <View style ={styles.container}>
+            <StatusBar
+            barStyle='light-content'
+            backgroundColor="transparent"
+            translucent
+            />
             <Image source={IllustrationImg}
             style={styles.image}
             />
-        <View style={styles.content}>
-            <Text style = { styles.tittle}>
-             Organize{'\n'}
-             suas jogatinas {'\n'}
-             facilmente
-            </Text>
-             <Text style = { styles.subtitle}>
-                Crei grupos para jogar seus games {'\n'}
-                favoritos com seus amigos 
-            </Text>
-
-        </View>
+            <View style={styles.content}>
+                <Text style = { styles.tittle}>
+                    Organize{'\n'}
+                    suas jogatinas {'\n'}
+                    facilmente
+                </Text>
+                <Text style = { styles.subtitle}>
+                    Crie grupos para jogar seus games {'\n'}
+                    favoritos com seus amigos 
+                </Text>
+            <ButtonIcon/>
+            </View>
         </View>
     )
 }
