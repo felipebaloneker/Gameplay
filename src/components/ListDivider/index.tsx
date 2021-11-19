ac5dd79725b0bcc39df45e1b25d24a0074d8840d
felipebaloneker <felipebaloneker23@gmail.com>
2021-11-19 17:49:21 -0300

    updated

---
+++ src/components/ListDivider/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import {
+    View
+} from 'react-native-view';
+import { styles } from 'react-native-styles';
+
+export function ListDivider(){
+    return(
+        <View
+        style={styles.container}
+        />
+    )
+}
