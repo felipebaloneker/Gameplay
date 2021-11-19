bacc33b43a27dcd9d083122cebef08e48a0c2bfb
Felipe <felipeBaloneker23@gmail.com>
2021-11-18 22:16:58 -0300

    criado components profile, Avatar pagina Home e rotas

--- App.tsx
+++ App.tsx
@@ -8,6 +8,7 @@ import {Rajdhani_500Medium, Rajdhani_700Bold} from '@expo-google-fonts/rajdhani'
 import AppLoading from 'expo-app-loading'
 
 import {SignIn}  from './src/screens/SignIn';
+import {Home}  from './src/screens/Home';
 import {Background} from './src/components/Background';
 
 export default function App() {
@@ -32,7 +33,7 @@ export default function App() {
       backgroundColor='transparent'
       translucent
       />
-          <SignIn/>  
+          <Home/>  
     </Background>
   );
 }
